ecd2c650dc1330858943673a9e911b9c2396c398
Mahesh Gadikota <gdmahesh8927@gmail.com>
2026-09-09 17:43:44 -0400

    Add Day 1 number classification

---
+++ Week1/Day1_NumberClassification/main.py
@@ -0,0 +1,11 @@
+# Day 1 - Number Classification
+# Check whether a number is positive, negative, or zero.
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
