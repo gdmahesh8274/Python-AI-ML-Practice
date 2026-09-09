730104318a060c7fd4291908d304168cbace7542
Mahesh Gadikota <gdmahesh8927@gmail.com>
2026-09-09 17:45:18 -0400

    Add Day 3 character frequency

---
+++ Week1/Day3_CharacterFrequency/main.py
@@ -0,0 +1,13 @@
+# Day 3 - Character Frequency
+
+text = input("Enter a string: ")
+frequency = {}
+
+for char in text:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+for char, count in frequency.items():
+    print(char, ":", count)
