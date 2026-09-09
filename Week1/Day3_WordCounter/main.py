41a0e35cb2b15fe4923b6669716914028e665358
Mahesh Gadikota <gdmahesh8927@gmail.com>
2026-09-09 17:45:13 -0400

    Add Day 3 word counter

---
+++ Week1/Day3_WordCounter/main.py
@@ -0,0 +1,9 @@
+# Day 3 - Word Counter
+
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+
+print("Number of words:", len(words))
+
+for word in words:
+    print(word, "length:", len(word))
